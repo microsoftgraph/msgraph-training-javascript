59081c93c7d3aed3be9235ff91b4ca70dbde300f
Jason Johnston <jasonjoh@users.noreply.github.com>
2024-08-08 14:38:09 -0400

    Update eslint

--- user-auth/graphtutorial/appSettings.example.js
+++ user-auth/graphtutorial/appSettings.example.js
@@ -1,16 +1,12 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
 // <SettingsSnippet>
 const settings = {
-  'clientId': 'YOUR_CLIENT_ID_HERE',
-  'tenantId': 'common',
-  'graphUserScopes': [
-    'user.read',
-    'mail.read',
-    'mail.send'
-  ]
+  clientId: 'YOUR_CLIENT_ID_HERE',
+  tenantId: 'common',
+  graphUserScopes: ['user.read', 'mail.read', 'mail.send'],
 };
 
-module.exports = settings;
+export default settings;
 // </SettingsSnippet>
